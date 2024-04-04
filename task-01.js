d901635639b772079a055ebee90d5f94c0fe0eb3
diana101010 <150705147+diana101010@users.noreply.github.com>
2024-04-04 18:02:49 +0300

    Update task-01.js

---
+++ task-01.js
@@ -0,0 +1,5 @@
+const categoriesList = document.querySelector("ul#categories");
+
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${categoriesItems.length}`);
